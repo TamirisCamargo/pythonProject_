a2b444aac90c12398d9eeabe2fd3efd4c0d6344f
Tamiris Camargo <tamiris-adm@hotmail.com>
2022-07-27 16:55:40 -0300

    New class

--- estudos_trainee/exercicio_frutas.py
+++ estudos_trainee/exercicio_frutas.py
@@ -15,7 +15,7 @@ ex: fruta = input('Qual fruta deseja comprar? ')
 """
 
 frutas = {
-    'Banana': 1.50,
+    'banana': 1.50,
     'Manga': 2.00,
     'Goiaba': 3.00,
     'Maçã': 1.55,
@@ -27,18 +27,28 @@ frutas = {
     'Kiwi': 7.00
 }
 
-escolha_fruta = input('Digite a fruta desejada: ')
-# print(escolha_fruta)
-
-# if escolha_fruta.lower() in frutas:
-
 for i in frutas:
-    if escolha_fruta.lower() in str(frutas).lower():
-        peso = input('Digite o peso desejado: ')
-        soma = float(peso) * float(frutas[escolha_fruta])
-        print(f'Obrigada, o valor total da fruta {escolha_fruta} é R$ {soma}')
-else:
-    print(f'Sorry, infelizmente não temos a fruta desejada. '
-          f' Segue nossa lista de frutas disponíveis {frutas}')
 
 
+    escolha_fruta = input('Digite a FRUTA desejada: ').lower()
+    # print(i)
+
+    # # print(frutas)
+
+
+    if escolha_fruta in frutas:
+
+        try:
+            peso = float(input('Digite o PESO desejado: '))
+
+        except Exception as erro:
+            print(f'O problema encontrado foi {erro.__class__}')
+        else:
+            soma = peso * float(frutas[escolha_fruta])
+            print(f'Obrigada, o valor total da fruta {escolha_fruta} é R$ {soma}')
+    else:
+        print(f'Sorry, infelizmente não temos a fruta desejada. \n'
+              f'Segue nossa lista de frutas disponíveis {frutas}')
+
+
+    # print(i)
